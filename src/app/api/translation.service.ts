cff91655022da61a133a2f13b2c98697cdc9befd
Radek Vala <radek.www@gmail.com>
2019-09-10 13:46:25 +0200

    Translation API service ADDED

---
+++ src/app/api/translation.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TranslationService {
+
+  constructor(private http: HttpClient) { }
+
+  getTranslation(text: String) {
+    return this.http.get('https://api.mymemory.translated.net/get?q='+text+'&langpair=en|cs');
+  }
+}
