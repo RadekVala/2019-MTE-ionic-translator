02cf03e8c4add7b231a54a98c23e7022ff1c1374
Radek Vala <radek.www@gmail.com>
2019-09-11 10:21:05 +0200

    Loading dialog ADDED

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslationService } from '../api/translation.service';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -10,8 +11,9 @@ export class Tab1Page {
 
   input:String = ''
   output: String = ''
+  loadingDialog: any
 
-  constructor(private api: TranslationService) {
+  constructor(private api: TranslationService, public loadingController: LoadingController) {
     // testing
     // this.input = 'K prekladu...';
   }
@@ -21,12 +23,25 @@ export class Tab1Page {
 
     // call a translation API
     if(this.input.length >= 2){
+
+      // show the loading dialog
+      this.presentLoading();
+
       this.api.getTranslation(this.input).subscribe( (data) => {
         console.log(data);
         this.output = data['responseData']['translatedText'];
+        // hide the loading dialog
+        this.loadingDialog.dismiss();
       });
     }
     
+  }
+
+  async presentLoading() {
+    this.loadingDialog = await this.loadingController.create({
+      message: 'Translating...',
+    });
+    await this.loadingDialog.present();
   }
 
 }
